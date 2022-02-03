6d3f977b4bfd1c4d5e1372a6813c51c18d1ca731
allison ruiz <allisonruiz@allisons-MacBook-Pro.local>
2022-02-02 21:29:57 -0800

    index.js

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const painting = document.querySelector('.painting')
 painting.addEventListener('click', function(){
     console.log('testing, testing, 123')
 })
+const painting = document.querySelector('.painting')
+painting.addEventListener('click', function(e){
+    console.log('testing, testing, 123')
+})
+const painting =document.querySelector('.painting')
+painting.addEventListener('click', function(e){
+    console.log(e.target)
+})
